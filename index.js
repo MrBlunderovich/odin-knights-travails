00d6e336ae59d7d7abd0670c67e7834e777ca2e3
Maxim <334890@gmail.com>
2023-06-14 11:05:42 +0600

    add basic css

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ import { knightMoves, notationToPath } from "./knightMoves.js";
 notationToPath("e6", "c4", knightMoves);
 notationToPath("e6", "f4", knightMoves);
 notationToPath("a1", "h7", knightMoves);
+
+const chessboard = document.querySelector(".chessboard");
